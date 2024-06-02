92da78e4793f01a9f7bae1d720ff01bdc96cdac9
ricardo acebal <richardrcmark.909@gmail.com>
2024-06-02 16:55:56 -0300

    arreglando mas cosas

--- persianas/settings.py
+++ persianas/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
