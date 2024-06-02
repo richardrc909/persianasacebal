74eb0dc6e80d4c1611baa1097edee18432023fef
ricardo acebal <richardrcmark.909@gmail.com>
2024-06-02 10:18:36 -0300

    configurando las keys

--- persianas/settings.py
+++ persianas/settings.py
@@ -9,21 +9,26 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import environ
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-74a)ac^&!z7e=&syr1=paqq83br9k#8@t)ty1a0se94x1ku5!y'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default='False')
 
 ALLOWED_HOSTS = []
 
